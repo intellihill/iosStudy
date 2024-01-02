5bb46515ad0f1883dadb6c39ed6dc2d203cf8ec3
정대로 <jundaero@jeongdaeloui-MacBookPro.local>
2024-01-03 05:58:31 +0900

    update: API study

---
+++ Concentrate/Concentration.swift
@@ -0,0 +1,19 @@
+//
+//  Concentration.swift
+//  Concentrate
+//
+//  Created by 정대로 on 1/3/24.
+//
+
+import Foundation
+
+/**
+ API : Application Programming Interface
+ 단지 클래스 안의 모든 메소드와 인스턴스 변수의 리스트이다
+ */
+class Concentration
+{
+    var cards: Array<Card>
+    
+    
+}
